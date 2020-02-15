fc2829eced13d0ec7298a9c8914cdc802e8a82af
sahilarora3117 <iamsahil@protonmail.com>
2020-02-15 15:03:22 +0530

    Recommendation system now online

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ app.use (bodyParser.json());
 app.use (bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'neo'))
+const driver = neo4j.driver('bolt://hobby-nmpibcmepbecgbkekclonkel.dbs.graphenedb.com:24787', neo4j.auth.basic("sahil", "b.0feMOs4Aj1EK.tryvQ0WA4BUhon0V"),{
+  encrypted:"ENCRYPTION_ON"
+});
 var session = driver.session();
 
 
@@ -42,7 +44,7 @@ app.post('/result', function(req,res){
   console.log(query);
   
   var books = [];
-  var quest = "CALL db.index.fulltext.queryNodes('books',"
+  var quest = "CALL db.index.fulltext.queryNodes('bookssearch',"
   quest +="'"+ query +"');"
 
   session
@@ -65,6 +67,26 @@ app.post('/result', function(req,res){
   app.post('/Recommend', function(req,res){
     var query = req.body.key;
     console.log(query);
+    var quest = 'MATCH(b:Book{isbn13:"'+query.trim()+'"})<-[:GENRES]-(g:Dgenre)-[:GENRES]->(rb:Book)<-[:GENRES]-(g1:Dgenre)-[:GENRES]->(rb2:Book) return rb2';
+    session
+    .run (quest)
+    .then(function(result){
+      var bookArray = [];
+      console.log(result.records);
+      result.records.forEach(function(records){
+        console.log(records._fields[0].properties.title);
+        bookArray.push({title:records._fields[0].properties.title, 
+                    authors: records._fields[0].properties.authors,
+                    ISBN13: records._fields[0].properties.isbn13});
+
+      });
+      console.log(bookArray);
+      res.render('recommend',{books: bookArray, quest: quest});
+    })
+    .catch(function(err){
+      console.log("Error connecting to the server.Contact Administrator");
+      res.render("index");
+      });
     
   });
 });
